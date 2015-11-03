eb52b5394660827399dfd0fd247742a33a22472a
Michael Carlson <michael.carlson@orbitz.com>
2015-11-03 10:41:55 -0600

    reworked how to get amazon queueUrl, had to set the region duh

--- src/main/java/com/orbitz/shadow/logging/amazonSQS.java
+++ src/main/java/com/orbitz/shadow/logging/amazonSQS.java
@@ -1,6 +1,9 @@
 package com.orbitz.shadow.logging;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.amazonaws.services.sqs.model.SendMessageRequest;
@@ -8,25 +11,36 @@ import com.amazonaws.services.sqs.model.SendMessageResult;
 
 public class amazonSQS {
 	AmazonSQS sqs;
+	String queueUrl;
 	
 	public amazonSQS(){
-		DefaultAWSCredentialsProviderChain dChain = new DefaultAWSCredentialsProviderChain();
+		DefaultAWSCredentialsProviderChain dChain = new DefaultAWSCredentialsProviderChain();		
+		ClientConfiguration cc = new ClientConfiguration();
+		Region region = Region.getRegion(Regions.US_WEST_2);
 		
+		//validating default credential chain is using correct credentials
 		System.out.println(dChain.getCredentials().getAWSAccessKeyId());
 		
-		this.sqs = new AmazonSQSClient(dChain.getCredentials());
+		
+		this.sqs = new AmazonSQSClient(dChain, cc);
+		sqs.setRegion(region);
+		
 		System.out.println("created queue client");
+		
+		//print existing queues
+		System.out.println(sqs.listQueues().getQueueUrls());
+		
+		queueUrl = sqs.createQueue("resultQueue").getQueueUrl();
+		
+		System.out.println("created/got queue " + queueUrl);
 	}
 	
 	//validate queue exists, add results to queue
 	public SendMessageResult sendMessage(String results) {
-		
-		System.out.println(sqs.listQueues().toString());
-		
-		//System.out.println(sqs.getQueueUrl("resultQueue").getQueueUrl());
-		
+				
 		SendMessageRequest smr = new SendMessageRequest()
-				.withQueueUrl("https://sqs.us-west-2.amazonaws.com/037156751156/resultQueue").withMessageBody(results);
+				.withQueueUrl(queueUrl).withMessageBody(results);
+		
 		return sqs.sendMessage(smr);
 		
 	}
